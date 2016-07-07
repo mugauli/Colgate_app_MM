e14ef15a89bd71ad5d58ab59c12fbdeb5544a084
Mugauli <mugauli2@hotmail.com>
2016-07-06 22:42:56 -0500

    Colgate

--- app/src/main/java/net/grapesoft/www/colgate/tusdatos.java
+++ app/src/main/java/net/grapesoft/www/colgate/tusdatos.java
@@ -1,13 +1,21 @@
-package net.grapesoft.colgate;
+package net.grapesoft.www.colgate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class tusdatos extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tusdatos);
+
+
+
     }
+
+
+
 }
