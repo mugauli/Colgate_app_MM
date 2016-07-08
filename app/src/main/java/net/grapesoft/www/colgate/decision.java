4ff18970ffefbd5480095770ef16522a2e259a83
Mugauli <mugauli2@hotmail.com>
2016-07-08 03:22:28 -0500

    Colgate tus datos

--- app/src/main/java/net/grapesoft/www/colgate/decision.java
+++ app/src/main/java/net/grapesoft/www/colgate/decision.java
@@ -1,7 +1,10 @@
 package net.grapesoft.www.colgate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class decision extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class decision extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decision);
+
+        LinearLayout llOpcion1 = (LinearLayout) findViewById(R.id.llOpcion1);
+
+        if (llOpcion1 != null) {
+            llOpcion1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(decision.this,caminoMPSS.class);
+                    startActivity(i);
+                }
+            });
+        }
+        LinearLayout llOpcion2 = (LinearLayout) findViewById(R.id.llOpcion2);
+        if (llOpcion2 != null) {
+            llOpcion2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(decision.this,video.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/net/grapesoft/www/colgate/video.java
+++ app/src/main/java/net/grapesoft/www/colgate/video.java
@@ -1,7 +1,12 @@
 package net.grapesoft.www.colgate;
 
+import android.media.session.MediaController;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class video extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = (VideoView) findViewById(R.id.VideoView);
+
+
+      // TextView tvTituloVideo = (TextView) findViewById(R.id.tvTituloVideo);
+      // tvTituloVideo.setText("El Régimen que va más con tu sonrisa es:");
+
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videocolgate));
+
+        videoView.setMediaController(new android.widget.MediaController(this));
+        videoView.start();
+
     }
 }
